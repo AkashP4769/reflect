7242fbfe5a9d0b47d5d68e02f61b742d8fe03a81
Akash <AwesomeAkash47@gmail.com>
2024-09-07 00:19:08 +0530

    Swipe feature

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -15,7 +15,7 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   late PageController? _pageController;
   double progress = 0;
-  double swipeup = 1;
+  bool visible = false;
 
   @override
   void initState() {
@@ -46,41 +46,70 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     final double height = MediaQuery.of(context).size.height;
-    return Scaffold(
-      body: Container(
-        height: height,
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            const Bg_Splash(),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: SingleChildScrollView(
-                child: Column( //wrap with scroll
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    const Reflect(),
-                    SizedBox(height: height * ((-0.4) * 1 + 0.5)),
-                    //swipe = 0  -> height = height * 0.5
-                    //swipe = 1 -> height = height * 0.1
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width,
-                      height: (480 + progress * 120) * 1,
-                      //duration: Duration(milliseconds: 400),
-                      child: PageView(
-                        controller: _pageController,
-                        children: [
-                          LoginCard(togglePage: togglePage,),
-                          SignUpCard(togglePage: togglePage,),
-                        ],
-                      ),
-                    )
-                  ],
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          height: height,
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              const Bg_Splash(),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: SingleChildScrollView(
+                  child: TweenAnimationBuilder(
+                    tween: Tween<double>(begin: visible ? 1 : 0, end: visible ? 0 : 1),
+                        duration: const Duration(milliseconds: 400),
+                        curve: Curves.fastOutSlowIn,
+                        builder: (BuildContext context, double value, Widget? child){
+                          return Column( //wrap with scroll
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              
+                              GestureDetector(
+                                onTap: (){
+                                  setState(() {
+                                    visible = !visible;
+                                  });
+                                },
+                                child: const Reflect()
+                              ),
+                              SizedBox(height: height * ((-0.4) * value + 0.5)),
+                              //swipe = 0  -> height = height * 0.5
+                              //swipe = 1 -> height = height * 0.1
+                              GestureDetector(
+                                onVerticalDragEnd: (details){
+                                  print(details.primaryVelocity);
+                                  if(details.primaryVelocity! > 200){
+                                    setState(() {
+                                      visible = !visible;
+                                    });
+                                  }
+                                },
+                                
+                                child: SizedBox(
+                                  width: MediaQuery.of(context).size.width,
+                                  height: (480 + progress * 120) * value,
+                                  //duration: Duration(milliseconds: 400),
+                                  child: PageView(
+                                    controller: _pageController,
+                                    children: [
+                                      LoginCard(togglePage: togglePage,),
+                                      SignUpCard(togglePage: togglePage,),
+                                    ],
+                                  ),
+                                ),
+                              )
+                            ],
+                          );
+                        }
+                        
+                  ),
                 ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
