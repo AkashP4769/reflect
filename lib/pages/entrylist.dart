7dca72581d34db65de63b21dc7d4249e0f3e2b2b
Akash <AwesomeAkash47@gmail.com>
2024-09-16 18:07:23 +0530

    entrylist page

--- lib/pages/entrylist.dart
+++ lib/pages/entrylist.dart
@@ -1,7 +1,9 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reflect/main.dart';
 import 'package:reflect/models/chapter.dart';
+import 'package:intl/intl.dart';
 
 class EntryListPage extends ConsumerStatefulWidget {
   final Chapter? chapter;
@@ -19,17 +21,56 @@ class _EntryListPageState extends ConsumerState<EntryListPage> {
     final themeData = ref.watch(themeManagerProvider);
     return Scaffold(
       body: Container(
-        padding: const EdgeInsetsDirectional.symmetric(horizontal: 20),
+        width: double.infinity,
+        //padding: const EdgeInsetsDirectional.symmetric(horizontal: 20),
         decoration: BoxDecoration(
           gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
+            begin: Alignment.bottomCenter,
+            end: Alignment.topCenter,
             colors: [themeData.colorScheme.tertiary, themeData.colorScheme.onTertiary]
           )
         ),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            ChapterHeader(chapter: chapter,),
+            SizedBox(height: 30,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.arrow_back, color: themeData.colorScheme.onPrimary,),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                ),
+                //const SizedBox(width: 10),
+                Expanded(
+                  child: Container(
+                    margin: const EdgeInsets.only(left: 10),
+                    height: 45,
+                    //width: double.infinity,
+                    child: SearchBar(
+                      backgroundColor: WidgetStateProperty.all(themeData.colorScheme.tertiary),
+                      elevation: WidgetStateProperty.all(0),
+                      trailing: [
+                        IconButton(
+                          onPressed: (){},
+                          icon: const Icon(Icons.search),
+                          color: themeData.colorScheme.onPrimary,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                IconButton(
+                  onPressed: (){}, 
+                  icon: Icon(Icons.menu, color: themeData.colorScheme.onPrimary,),
+                )
+              ],
+            ),
+
+            const SizedBox(height: 20),
+            ChapterHeader(chapter: chapter, themeData: themeData,),
             /*Expanded(
               child: ListView.builder(
                 itemCount: 10,
@@ -50,12 +91,52 @@ class _EntryListPageState extends ConsumerState<EntryListPage> {
 
 class ChapterHeader extends StatelessWidget {
   final ChapterAdvanced chapter;
-  const ChapterHeader({super.key, required this.chapter});
+  final ThemeData themeData;
+  const ChapterHeader({super.key, required this.chapter, required this.themeData});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      width: double.infinity,
+      padding: const EdgeInsetsDirectional.symmetric(horizontal: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          
+
+          if(chapter.imageUrl != null) ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: CachedNetworkImage(imageUrl: chapter.imageUrl ?? "", width: double.infinity, height: 200, fit: BoxFit.cover),
+          ),
+          const SizedBox(height: 20),
+          Text(chapter.title, style: themeData.textTheme.titleLarge?.copyWith(color: const Color(0xffFF9432),),),
+          const SizedBox(height: 10),
+          Text(chapter.description, style: themeData.textTheme.bodyMedium?.copyWith(color: themeData.colorScheme.onPrimary, fontWeight: FontWeight.w600,), textAlign: TextAlign.center,),
+          const SizedBox(height: 20),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Icon(Icons.book, color: themeData.colorScheme.onPrimary,),
+                  const SizedBox(width: 5),
+                  Text('No. of entries - ${chapter.entryCount}', style: themeData.textTheme.bodySmall?.copyWith(color: themeData.colorScheme.onPrimary),)
+                ],
+              ),
+              Row(
+                children: [
+                  Icon(Icons.lock_clock, color: themeData.colorScheme.onPrimary,),
+                  const SizedBox(width: 5),
+                  Text('Created on - ${DateFormat('dd/MM/yyyy').format(chapter.createdOn)}', style: themeData.textTheme.bodySmall?.copyWith(color: themeData.colorScheme.onPrimary),)
+                ],
+              )
+            ],
+          ),
+          Divider(color: themeData.colorScheme.onPrimary, thickness: 1, height: 30),
+
+        ],
+      ),
     );
   }
 }
